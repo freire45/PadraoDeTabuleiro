c09cf64a0cc942ce0b24eb3873d1e0fa3641d212
Erick Freire <erilefr@gmail.com>
2022-10-17 22:26:35 -0300

    versao1
    versao1

---
+++ PadraoDeTabuleiro/src/br/com/erickfreire/padraodetabuleiro/PadraoDeTabuleiro.java
@@ -0,0 +1,17 @@
+package br.com.erickfreire.padraodetabuleiro;
+
+public class PadraoDeTabuleiro {
+
+	public static void main(String[] args) {
+		System.out.printf("* * * * * * * *  %n"
+				        + " * * * * * * * * %n"
+				        + "* * * * * * * *  %n"
+				        + " * * * * * * * * %n"
+				        + "* * * * * * * *  %n"
+				        + " * * * * * * * * %n"
+				        + "* * * * * * * *  %n"
+				        + " * * * * * * * * %n");
+
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PadraoDeTabuleiro
  Programa em Java que simula um padrão de tabuleiro com asteriscos
+
+## Image
+ ![Padrão de tabuleiro com Asteriscos](padraodetabuleiro.png)
